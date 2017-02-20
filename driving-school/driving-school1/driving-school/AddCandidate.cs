9d8ec97a376ed46ffb9e3448e9b1d823b063db62
Nemanja4 <sada4sada4@gmail.com>
2017-02-20 08:59:15 +0100

    Forme za dodavanje i prikaz kandidata

--- driving-school/driving-school1/driving-school/AddCandidate.cs
+++ driving-school/driving-school1/driving-school/AddCandidate.cs
@@ -13,8 +13,7 @@ namespace driving_school
 {
     public partial class AddCandidate : Form
     {
-        public static string path = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\w7\Desktop\driving-school1\driving-school\Databas.mdf;Integrated Security=True;Encrypt=False;User Instance=False;Context Connection=False";
-
+        
         public AddCandidate()
         {
             InitializeComponent();
@@ -22,35 +21,37 @@ namespace driving_school
 
         private void AddCandidate_Load(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(AddCandidate.path);
+            SqlConnection con = new SqlConnection(Form1.path);
 
             try
             {
+
+                con.Close();
                 con.Open();
+                SqlCommand command = con.CreateCommand();
+
+                command.CommandText = "SELECT * FROM municipalities ";
+
+                SqlDataReader rdr = command.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Municipalities.Items.Add(rdr.GetString(1));
+                }
+                con.Close();
             }
             catch (Exception ee)
             {
-                MessageBox.Show("Connection has failed");
+                MessageBox.Show("Connection has failed\n" + ee.ToString());
                 return;
             }
-
-            SqlCommand command = con.CreateCommand();
-
-            command.CommandText = "SELECT * FROM municipalities ";
-
-            SqlDataReader rdr = command.ExecuteReader();
-
-            while (rdr.Read())
-            {
-                Municipalities.Items.Add(rdr.GetString(1));
-            }
-
 
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int broj;
             if (FirstName.Text == "")
             {
                 MessageBox.Show("you did not enter the first name of the candidate !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -86,7 +87,7 @@ namespace driving_school
                 return;
             }
 
-            else if (PhoneNumber.Text == "")
+            else if (PhoneNumber.Text == "" && Int32.TryParse(PhoneNumber.Text, out broj))
             {
                 MessageBox.Show("you did not enter the phone number of the candidate !", "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 PhoneNumber.Focus();
@@ -98,52 +99,43 @@ namespace driving_school
                 MessageBox.Show("you did not enter the municipalities of the candidate !", "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
-            SqlConnection con = new SqlConnection(AddCandidate.path);
-
-            try
+            else
             {
-                con.Open();
+                SqlConnection con = new SqlConnection(Form1.path);
+
+                try
+                {
+                    con.Close();
+                    con.Open();
+
+                    SqlCommand command = con.CreateCommand();
+
+                    command.CommandText = "SELECT Id FROM municipalities WHERE name = '" + Municipalities.SelectedItem.ToString() + "';";
+
+                    SqlDataReader rdr = command.ExecuteReader();
+                    rdr.Read();
+                    int d = rdr.GetInt32(0);
+                    rdr.Close();
+
+                    command.CommandText = "INSERT INTO candidate ( municipalities_id, first_name, last_name, unique_identifacion_number, Id_number, address, phone_number) VALUES ( " + d + ",'" + FirstName.Text + "','" + LastName.Text + "','" + UniqueIN.Text + "','" + IDNumber.Text + "','" + Address.Text + "'," + int.Parse(PhoneNumber.Text) + ");";
+                   command.ExecuteNonQuery();
+                    MessageBox.Show("Successfully entered candidate");
+                        FirstName.Clear();
+                        LastName.Clear();
+                        UniqueIN.Clear();
+                        IDNumber.Clear();
+                        Address.Clear();
+                        PhoneNumber.Clear();
+                        Municipalities.SelectedIndex = -1;
+                    con.Close();
+
+                }
+                catch (Exception ee)
+                {
+                    MessageBox.Show("" + ee.ToString());
+                    return;
+                }
             }
-            catch (Exception ee)
-            {
-                MessageBox.Show("Connection has failed");
-                return;
-            }
-
-            SqlCommand command = con.CreateCommand();
-
-            command.CommandText = "SELECT Id FROM municipalities WHERE name = '" + Municipalities.Text + "';";
-
-            SqlDataReader rdr = command.ExecuteReader();
-            rdr.Read();
-            int d = rdr.GetInt32(0);
-            rdr.Close();
-
-            command.CommandText = "INSERT INTO candidate ( municipalities_id, first_name, last_name, unique_identifacion_number, Id_number, address, phone_number) VALUES ( " + d + ",'" + FirstName.Text + "','" + LastName.Text + "'," + UniqueIN.Text + ",'" + IDNumber.Text + "','" + Address.Text + "'," + PhoneNumber.Text + ");";
-            try
-            {
-                command.ExecuteNonQuery();
-            }
-
-            catch (Exception ee)
-            {
-                MessageBox.Show(ee.Message);
-
-                con.Close();
-                return;
-            }
-            MessageBox.Show("Successfully entered candidate");
-            FirstName.Clear();
-            LastName.Clear();
-            UniqueIN.Clear();
-            IDNumber.Clear();
-            Address.Clear();
-            PhoneNumber.Clear();
-            Municipalities.SelectedIndex = -1;
-            con.Close();
-
-
+        }
     }
-}
 }
